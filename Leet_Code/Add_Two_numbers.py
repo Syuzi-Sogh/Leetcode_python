fc1b4894f741729b301eab9b18ebc36de9af108a
Syuzi <sogomonansuzi@gmail.com>
2026-05-06 12:36:05 +0500

    update for Add_Two_numbers

--- Leet_Code/Add_Two_numbers.py
+++ Leet_Code/Add_Two_numbers.py
@@ -1,7 +1,3 @@
-#Input: l1 = [2,4,3], l2 = [5,6,4]
-# Output: [7,0,8]
-# Explanation: 342 + 465 = 807.
-
 lst1 = [2, 4, 3]
 lst2 = [5, 6, 4]
 
@@ -19,9 +15,6 @@ for i in range(len(lst1)):
         result.append(s % 10)
     else:
         result.append(s)
-
-    # if (i + 1) == len(lst1):
-    #     result.append(cntr)
 
 
 if cntr == 1:
